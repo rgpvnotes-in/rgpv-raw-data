d8174ef60f3b391aebf0a7a11b6137291987ce06
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-03-18 03:19:36 +0530

    minor fix

--- services/auth/index.js
+++ services/auth/index.js
@@ -4,7 +4,10 @@ exports.imageGeneratorAuth = (() => {
   try {
     const randomGradient = Math.floor(Math.random() * 5) + 1;
     console.log('imageGeneratorAuth is using number: ', randomGradient);
-    return JSON.parse(IMAGE_GENERATOR_AUTH[randomGradient]);
+    return JSON.parse(
+      process.env.IMAGE_GENERATOR_AUTH[randomGradient] ||
+        process.env.IMAGE_GENERATOR_AUTH_1,
+    );
   } catch (error) {
     console.error('something went wrong in imageGeneratorAuth ', error);
   }

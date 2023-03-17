1495ebd03faceb30321419a59b346fac594d4a3d
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-03-18 03:34:50 +0530

    will thix fix that

--- services/imageGenerator/index.js
+++ services/imageGenerator/index.js
@@ -6,27 +6,27 @@ const backgroundImage =
 
 const linearGradient = (() => {
   const linearGradientArray = [
-    'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);',
-    'linear-gradient(135deg, #7474BF 0%, #348AC7 100%);',
-    'linear-gradient(135deg, #DC2424 0%, #4A569D 100%);',
-    'linear-gradient(135deg, #232526 0%, #414345 100%);',
-    'linear-gradient(135deg, #4776E6 0%, #8E54E9 100%);',
-    'linear-gradient(135deg, #614385 0%, #516395 100%);',
-    'linear-gradient(135deg, #16222A 0%, #3A6073 100%);',
-    'linear-gradient(135deg, #EB3349 0%, #F45C43 100%);',
-    'linear-gradient(135deg, #FF512F 0%, #F09819 100%);',
-    'linear-gradient(135deg, #e52d27 0%, #b31217 100%);',
-    'linear-gradient(135deg, #2b5876 0%, #4e4376 100%);',
-    'linear-gradient(135deg, #1488CC 0%, #2B32B2 100%);',
-    'linear-gradient(135deg, #b92b27 0%, #1565C0 100%);',
-    'linear-gradient(135deg, #373B44 0%, #4286f4 100%);',
-    'linear-gradient(135deg, #8E2DE2 0%, #4A00E0 100%);',
-    'linear-gradient(135deg, #f953c6 0%, #b91d73 100%);',
-    'linear-gradient(135deg, #c31432 0%, #240b36 100%);',
-    'linear-gradient(135deg, #f12711 0%, #f5af19 100%);',
-    'linear-gradient(135deg, #8360c3 0%, #2ebf91 100%);',
-    'linear-gradient(135deg, #FF512F 0%, #DD2476 100%);',
-    'linear-gradient(135deg, #FF512F 0%, #DD2476 100%);',
+    'linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)',
+    'linear-gradient(135deg, #7474BF 0%, #348AC7 100%)',
+    'linear-gradient(135deg, #DC2424 0%, #4A569D 100%)',
+    'linear-gradient(135deg, #232526 0%, #414345 100%)',
+    'linear-gradient(135deg, #4776E6 0%, #8E54E9 100%)',
+    'linear-gradient(135deg, #614385 0%, #516395 100%)',
+    'linear-gradient(135deg, #16222A 0%, #3A6073 100%)',
+    'linear-gradient(135deg, #EB3349 0%, #F45C43 100%)',
+    'linear-gradient(135deg, #FF512F 0%, #F09819 100%)',
+    'linear-gradient(135deg, #e52d27 0%, #b31217 100%)',
+    'linear-gradient(135deg, #2b5876 0%, #4e4376 100%)',
+    'linear-gradient(135deg, #1488CC 0%, #2B32B2 100%)',
+    'linear-gradient(135deg, #b92b27 0%, #1565C0 100%)',
+    'linear-gradient(135deg, #373B44 0%, #4286f4 100%)',
+    'linear-gradient(135deg, #8E2DE2 0%, #4A00E0 100%)',
+    'linear-gradient(135deg, #f953c6 0%, #b91d73 100%)',
+    'linear-gradient(135deg, #c31432 0%, #240b36 100%)',
+    'linear-gradient(135deg, #f12711 0%, #f5af19 100%)',
+    'linear-gradient(135deg, #8360c3 0%, #2ebf91 100%)',
+    'linear-gradient(135deg, #FF512F 0%, #DD2476 100%)',
+    'linear-gradient(135deg, #FF512F 0%, #DD2476 100%)',
   ];
   const randomGradient = Math.floor(Math.random() * linearGradientArray.length);
   return linearGradientArray[randomGradient];
@@ -36,9 +36,9 @@ const imageTextFontSize = (postCaption) => {
   const postCaptionLength = postCaption.length;
   let postCaptionFontSize = '75px';
   if (postCaptionLength < 65) {
-    postCaptionFontSize = '130px;';
+    postCaptionFontSize = '130px';
   } else if (postCaptionLength < 170) {
-    postCaptionFontSize = '100px;';
+    postCaptionFontSize = '100px';
   }
   return postCaptionFontSize;
 };
@@ -80,8 +80,8 @@ exports.postImageUrl = async (postCaption) => {
     const generateHtmlForImage = htmlForImage(postCaption);
 
     const imageOptions = {
-      html: generateCssForImage,
-      css: generateHtmlForImage,
+      html: generateHtmlForImage,
+      css: generateCssForImage,
       google_fonts: 'Roboto',
     };
 

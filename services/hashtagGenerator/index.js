402b2045370832d0a30bc58cbc2247824d7a69df
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-03-19 13:47:52 +0530

    using exports instead of export

--- services/hashtagGenerator/index.js
+++ services/hashtagGenerator/index.js
@@ -1,2 +1,2 @@
-export const constantHashTag =
+exports.constantHashTag =
   '#rgpv #rgpvalerts #rgpvbhopal #rgpvnotes #education #notestagram #theengineerbro #exam #engineeringcollege #engineeringstudents';

11e1c4eb249065fc58c8b559ef4ef6ceb836edfa
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-03-19 13:51:30 +0530

    adding fall back image generator auth

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
-export const imageGeneratorAuth = (() => {
+exports.imageGeneratorAuth = (() => {
   try {
     const randomGradient = Math.floor(Math.random() * 5) + 1;
-    return JSON.parse(IMAGE_GENERATOR_AUTH[randomGradient]);
+    console.log('imageGeneratorAuth is using number: ', randomGradient);
+    return (
+      process.env[`IMAGE_GENERATOR_AUTH_${randomGradient}`] ||
+      process.env.IMAGE_GENERATOR_AUTH_1
+    );
   } catch (error) {
     console.error('something went wrong in imageGeneratorAuth ', error);
   }
